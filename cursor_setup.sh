2509cadd6ca53ea3cd2324e5b938f6d2325ee248
Bharath Babu R <61000612+bharath93-byte@users.noreply.github.com>
2026-08-05 10:16:27 +0530

    setup_script_added

---
+++ cursor_setup.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/.cursor/rules
+
+cat ./aidlc-rules/aws-aidlc-rules/core-workflow.md >> ~/.cursor/rules/ai-dlc-workflow.mdc
+
+mkdir -p ~/.cursor/.aidlc-rule-details
+
+cp -R ./aidlc-rules/aws-aidlc-rule-details/* ~/.cursor/.aidlc-rule-details/
